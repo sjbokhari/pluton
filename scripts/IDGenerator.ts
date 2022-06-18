8aa8152d880796e05d5b2c09ac901cfabb099b41
sjbokhari0 <60392358+sjbokhari0@users.noreply.github.com>
2022-06-19 01:22:54 +0200

    corrected baremetal deploy

--- scripts/IDGenerator.ts
+++ scripts/IDGenerator.ts
@@ -1,3 +1,5 @@
+// https://zelark.github.io/nano-id-cc/
+// https://github.com/ai/nanoid
 // const { customAlphabet } = require('nanoid')
 // const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 // const nanoid = customAlphabet(alphabet, 24)
