da2011b554bc74ada2c24b21f9a6bf54318dc9ea
sjbokhari0 <60392358+sjbokhari0@users.noreply.github.com>
2023-05-02 10:45:25 +0200

    work in progress

--- api/database.py
+++ api/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgresserver/db"
 
 engine = create_engine(
     # connect_args only for SQLite. It's not needed for other databases.
